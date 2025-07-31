34efd0f6c909e62fae66a9a7919a84c2b6f3d624
valentina23-ai <vogueosorio23@gmail.com>
2025-07-31 12:26:02 -0500

    Repositorio js versión 1

---
+++ 1_sintaxis_variable/app.js
@@ -0,0 +1,29 @@
+//El alert muestra una ventana emergente en el navegador//
+alert ("hola mundo")
+//El prompt muestra una caja para ingresar informacion desde el navegador//
+prompt("Dime tu nombre")
+//variable declarada con let//
+let personaje = "tomy"
+//cxrear variable puntaje y asignar valor de 0//
+let puntaje = 0
+//Crear variable shots y asignar valor de 3//
+let shots = 3
+    recargar = 2
+
+console.log("total de balas:",shots + recargar)
+
+//El usuario capturo 4 patos//
+puntaje += 4
+//El usuario gasto 2 balas//
+shots -=2
+//El usuario recargo su arma//
+recargar +=1
+
+console.log("puntaje",puntaje)
+
+console.log("shots",shots)
+
+console.log("recargar",recargar)
+
+ 
+ 
---
+++ 2_operadores_comparación_logica/app1.js
@@ -0,0 +1,48 @@
+//boolean(...)
+
+console.log(' "5" === "5" ', "5" == "5")//true
+console.log(' "5" === "5" ', "5" === "5")//true
+console.log(' 5 === "5" ', 5 === "5")//false
+console.log(' 5 === "5" ', 5 == "5")//true
+console.log(' 5 === (3+2) ', "5" ===  (3+2))//false
+console.log(' 5 === 5 ', 5 === 5)//true
+
+//concatenar (unir) (+)
+console.log(5 + "3")//53
+console.log(5 * "3")//15
+console.log(5 + 3)//8
+
+// pida dos números al usuario y sumelos//
+let num1 = prompt('dame num1') // "5"
+let num2 = prompt('dame num2') //"7"
+let resultado= num1 + num2
+alert("el rersultado de tu suma es " + resultado)
+
+//TRABAJO//
+// Pedir los valores al usuario
+let a =Number( prompt("Ingresa el número A:"));
+let b =Number(prompt("Ingresa el número B:"));
+
+
+// Hacemos las operaciones
+let suma = a + b;
+let resta = a - b;
+let multiplicación = a * b;
+let division = a / b;
+
+// Mostramos los resultados con alert
+alert("Tú escribiste:\nA = " + a + "\nB = " + b);
+alert("La suma es: " + suma);
+alert("La resta es: " + resta);
+alert("La multiplicación es: " + multiplicación);
+alert("La división es: " + division);
+
+// También mostramos los resultados en la consola
+console.log("A =", a);
+console.log("B =", b);
+console.log("Suma =", suma);
+console.log("Resta =", resta);
+console.log("Multiplicación =", multiplicación);
+console.log("División =", división);
+
+
