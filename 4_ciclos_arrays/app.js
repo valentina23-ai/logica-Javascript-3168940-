545086a424e82b88673898190aa6a4ba18981b68
valentina23-ai <vogueosorio23@gmail.com>
2025-08-06 12:33:11 -0500

    4_ciclos_arrays

---
+++ 4_ciclos_arrays/app.js
@@ -0,0 +1,23 @@
+//Ejemplos
+//Array
+let frutas = ["mango","piña","papaya","uva","limón","aguacate"]
+let jugadores = [
+  {
+    id: 1,
+    nombre: "diaz", 
+    pais: "col"
+  },
+    "james",
+    "messi"
+]
+console.log( jugadores[0].nombre ) //Imprimir nombre "diaz"
+
+//Objeto
+let pelicula = {
+  nombre: "Back to the future",
+  estreno: 1986,
+  director:  "Robert Zemeckis",
+  calificacion: "8.5"
+}
+
+console.log( pelicula.director )
